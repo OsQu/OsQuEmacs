69ef9d5197a69f4354c0be3b2e84239e93651ab4
Oskari Virtaoja <oskari@smartly.io>
2019-06-17 21:06:15 +0300

    Add rest of the logging functions

--- org-tracker/tracker.el
+++ org-tracker/tracker.el
@@ -1,13 +1,15 @@
 ;;; ~/.doom.d/org-tracker/export_csv.el -*- lexical-binding: t; -*-
-(defun org-tracker-start-task ()
+(defun org-tracker-task-start ()
   (interactive)
-  (let ((timestamp (format-time-string
-                     "%D"
-                     (org-read-date
-                      nil t nil "Enter start date" nil)
-                     )))
-    (org-set-property "STARTED_AT" timestamp))
-  )
+  (org-tracker-prompt-time "STARTED_AT"))
+
+(defun org-tracker-task-pr ()
+  (interactive)
+  (org-tracker-prompt-time "PR_AT"))
+
+(defun org-tracker-task-done ()
+  (interactive)
+  (org-tracker-prompt-time "DONE_AT"))
 
 
 (defun org-tracker-prompt-time (property)
