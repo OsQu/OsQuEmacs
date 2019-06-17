05f7b380eb7910b76becfc69b39b24894e4eacd9
Oskari Virtaoja <oskari@smartly.io>
2019-06-17 22:35:50 +0300

    Human readable properties for prompt

--- org-tracker/tracker.el
+++ org-tracker/tracker.el
@@ -1,23 +1,23 @@
 ;;; ~/.doom.d/org-tracker/export_csv.el -*- lexical-binding: t; -*-
 (defun org-tracker-task-start ()
   (interactive)
-  (org-tracker-prompt-time "STARTED_AT"))
+  (org-tracker-prompt-time "STARTED_AT" "Started at"))
 
 (defun org-tracker-task-pr ()
   (interactive)
-  (org-tracker-prompt-time "PR_AT"))
+  (org-tracker-prompt-time "PR_AT" "Moved to PR at"))
 
 (defun org-tracker-task-done ()
   (interactive)
-  (org-tracker-prompt-time "DONE_AT"))
+  (org-tracker-prompt-time "DONE_AT" "Done at"))
 
 
-(defun org-tracker-prompt-time (property)
+(defun org-tracker-prompt-time (property readable-property)
   "Set property into a prompted time"
   (let ((timestamp (format-time-string
                      "%D"
                      (org-read-date
-                      nil t nil "Enter start date" nil)
+                      nil t nil (concat "Enter " readable-property ":") nil)
                      )))
     (org-set-property property timestamp))
   )
